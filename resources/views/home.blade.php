22097b0c05bde1db33663e32d1942e3f95f3ed4b
puazgit <i.apriyampon@gmail.com>
2019-08-01 21:04:53 +0700

    before add roles spatie

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts/wrapper')
 {{-- @extends('layouts.app') --}}
-
 @section('content')
                     <!-- BEGIN PAGE TITLE-->
 <h3 class="page-title"> Blank Page Layout
@@ -9,6 +8,7 @@
     <!-- END PAGE TITLE-->
     <!-- END PAGE HEADER-->
     <div class="note note-info">
-        <p> A black page template with a minimal dependency assets to use as a base for any custom page you create </p>
+        <p>
+                {{ auth()->user()->getRoleNames()->first() }} A black page template with a minimal dependency assets to use as a base for any custom page you create </p>
     </div>
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -306,7 +306,7 @@
                     <li class="dropdown dropdown-user">
                         <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
                             <img alt="" class="img-circle" src="{{asset ('assets/img/avatar3_small.jpg')}}" />
-                            <span class="username username-hide-on-mobile"> Nick </span>
+                            <span class="username username-hide-on-mobile"> {{ Auth::user()->name }} </span>
                             <i class="fa fa-angle-down"></i>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-default">
