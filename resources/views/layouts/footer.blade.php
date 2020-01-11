9bf80c036f30c0e4ce9cc2d30a681b5caedcba7c
puaz <i.apriyampon.com>
2020-01-11 23:12:49 +0700

    edit notifikasi (start)

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="page-footer">
-        <div class="page-footer-inner"> 2019 &copy; Perum Jasa Tirta II</div>
-        <div class="scroll-to-top">
-            <i class="icon-arrow-up"></i>
-        </div>
+    <div class="page-footer-inner"> 2020 &copy; Perum Jasa Tirta II</div>
+    <div class="scroll-to-top">
+        <i class="icon-arrow-up"></i>
     </div>
+</div>
--- resources/views/layouts/wrapper.blade.php
+++ resources/views/layouts/wrapper.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Metronic | Perum Jasa Tirta II</title>
+    <title>SI RTM | Perum Jasa Tirta II</title>
     <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport" />
